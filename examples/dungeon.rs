09aaf7acae7923adc70acd0ce90e5eca8b6c6838
Florian Blasius <flovanpt@posteo.de>
2019-01-21 16:17:20 +0100

    [update] use orbtk messages

--- examples/dungeon.rs
+++ examples/dungeon.rs
@@ -10,46 +10,16 @@ fn create_button_with_container(label: &str, handler: MouseEventHandler) -> Temp
     )
 }
 
-// todo: better way in OrbTk to act with parent state.
-fn create_menu(new_game: MouseEventHandler, quit: MouseEventHandler) -> Template {
-    let visibility = SharedProperty::new(Visibility::Visible);
-
-    Template::default()
-        .as_parent_type(ParentType::Single)
-        .with_debug_name("MenuView")
-        .with_child(
-            Center::create()
-                .as_parent_type(ParentType::Single)
-                .with_child(
-                    Container::create()
-                        .with_property(Selector::from("container").with_class("menu"))
-                        .with_child(
-                            Column::create()
-                                .with_child(
-                                    create_button_with_container("New Game", new_game)
-                                        .with_shared_property(visibility.clone()),
-                                )
-                                .with_child(
-                                    create_button_with_container("Quit", quit)
-                                        .with_shared_property(visibility.clone()),
-                                ),
-                        ),
-                ),
-        )
-        .with_property(Selector::default().with_id("menu-view"))
-        .with_shared_property(visibility)
-}
-
 #[derive(Default)]
-struct GameViewState {
-    game_started: Cell<bool>,
+struct MenuViewState {
+    start_game: Cell<bool>,
     close_game: Cell<bool>,
 }
 
-impl GameViewState {
+impl MenuViewState {
     fn start_game(&self) {
         println!("Start game");
-        self.game_started.set(true);
+        self.start_game.set(true);
     }
 
     fn quit_game(&self) {
@@ -58,15 +28,82 @@ impl GameViewState {
     }
 }
 
-impl State for GameViewState {
+impl State for MenuViewState {
     fn update(&self, context: &mut Context<'_>) {
         if self.close_game.get() {
-            context.push_event(SystemEvent::Quit);
+            context.send_message("game_view", StringMessage::from("quit"));
         }
-        if let Some(menu_view) = &mut context.widget_from_id("menu-view") {
-            if self.game_started.get() {
-                if let Ok(visibility) = menu_view.borrow_mut_property::<Visibility>() {
-                    *visibility = Visibility::Collapsed
+
+        if self.start_game.get() {
+            context.send_message("game_view", StringMessage::from("start"));
+        }
+    }
+}
+
+struct MenuView;
+
+impl Widget for MenuView {
+    fn create() -> Template {
+        let state = Rc::new(MenuViewState::default());
+        let ng_state = state.clone();
+        let q_state = state.clone();
+
+        Template::default()
+            .as_parent_type(ParentType::Single)
+            .with_debug_name("MenuView")
+            .with_child(
+                Center::create()
+                    .as_parent_type(ParentType::Single)
+                    .with_child(
+                        Container::create()
+                            .with_property(Selector::from("container").with_class("menu"))
+                            .with_child(
+                                Column::create()
+                                    .with_child(create_button_with_container(
+                                        "New Game",
+                                        MouseEventHandler::default().on_click(Rc::new(
+                                            move |_pos: Point| -> bool {
+                                                ng_state.start_game();
+                                                true
+                                            },
+                                        )),
+                                    ))
+                                    .with_child(create_button_with_container(
+                                        "Quit",
+                                        MouseEventHandler::default().on_click(Rc::new(
+                                            move |_pos: Point| -> bool {
+                                                q_state.quit_game();
+                                                true
+                                            },
+                                        )),
+                                    )),
+                            ),
+                    ),
+            )
+            .with_property(Selector::default().with_id("menu-view"))
+            .with_state(state)
+    }
+}
+
+#[derive(Default)]
+struct GameViewState {}
+
+impl State for GameViewState {
+    fn receive_messages(&self, context: &mut Context<'_>, messages: &Vec<MessageBox>) {
+        for message in messages {
+            if let Ok(message) = message.downcast_ref::<StringMessage>() {
+                match message.0.as_str() {
+                    "start" => {
+                        if let Some(menu_view) = &mut context.widget_from_id("menu-view") {
+                            if let Ok(visibility) = menu_view.borrow_mut_property::<Visibility>() {
+                                *visibility = Visibility::Collapsed
+                            }
+                        }
+                    }
+                    "quit" => {
+                        context.push_event(SystemEvent::Quit);
+                    }
+                    _ => {}
                 }
             }
         }
@@ -78,8 +115,6 @@ struct GameView;
 impl Widget for GameView {
     fn create() -> Template {
         let state = Rc::new(GameViewState::default());
-        let ng_state = state.clone();
-        let q_state = state.clone();
 
         Template::default()
             .as_parent_type(ParentType::Single)
@@ -90,21 +125,9 @@ impl Widget for GameView {
                         Container::create()
                             .with_child(TextBlock::create().with_property(Label::from("Dungeon"))),
                     )
-                    .with_child(Container::create().with_child(create_menu(
-                        MouseEventHandler::default().on_click(Rc::new(
-                            move |_pos: Point| -> bool {
-                                ng_state.start_game();
-                                true
-                            },
-                        )),
-                        MouseEventHandler::default().on_click(Rc::new(
-                            move |_pos: Point| -> bool {
-                                q_state.quit_game();
-                                true
-                            },
-                        )),
-                    ))),
+                    .with_child(MenuView::create()),
             )
+            .with_property(Selector::default().with_id("game_view"))
             .with_state(state)
     }
 }

a616059faf4a0fc1a0da2ac168dd332d0b04d5fd
Florian Blasius <flovanpt@posteo.de>
2019-04-02 19:07:08 +0200

    [code cleanup] dungeon example.

--- examples/dungeon.rs
+++ examples/dungeon.rs
@@ -1,152 +1,142 @@
-
 use orbgame::prelude::*;
+use std::cell::Cell;
 
-mod menu {
-    use orbgame::prelude::*;
-    use std::cell::Cell;
+#[derive(Default)]
+pub struct MenuViewState {
+    start_game: Cell<bool>,
+    close_game: Cell<bool>,
+}
 
-    #[derive(Default)]
-    struct MenuViewState {
-        start_game: Cell<bool>,
-        close_game: Cell<bool>,
+impl MenuViewState {
+    fn start_game(&self) {
+        println!("Start game");
+        self.start_game.set(true);
     }
 
-    impl MenuViewState {
-        fn start_game(&self) {
-            println!("Start game");
-            self.start_game.set(true);
+    fn quit_game(&self) {
+        println!("Quit game");
+        self.close_game.set(true);
+    }
+}
+
+impl State for MenuViewState {
+    fn update(&self, context: &mut Context<'_>) {
+        if self.close_game.get() {
+            context.send_message("game_view", StringMessage::from("quit"));
         }
 
-        fn quit_game(&self) {
-            println!("Quit game");
-            self.close_game.set(true);
+        if self.start_game.get() {
+            context.send_message("game_view", StringMessage::from("start"));
         }
     }
+}
 
-    impl State for MenuViewState {
-        fn update(&self, context: &mut Context<'_>) {
-            if self.close_game.get() {
-                context.send_message("game_view", StringMessage::from("quit"));
-            }
-
-            if self.start_game.get() {
-                context.send_message("game_view", StringMessage::from("start"));
-            }
-        }
-    }
-
-    widget!(
+widget!(
         MenuView<MenuViewState> {
              selector: Selector
         }
     );
 
-    impl Template for MenuView {
-        fn template(self, id: Entity, context: &mut BuildContext) -> Self {
-            let state = self.clone_state();
-            let ng_state = state.clone();
-            let q_state = state.clone();
+impl Template for MenuView {
+    fn template(self, id: Entity, context: &mut BuildContext) -> Self {
+        let state = self.clone_state();
+        let ng_state = state.clone();
+        let q_state = state.clone();
 
-            self.name("MenuView")
-                .selector(Selector::default().id("menu_view"))
-                .child(
-                    Grid::create()
-                        .selector(Selector::from("grid").class("start"))
-                        .child(
-                            Container::create()
-                                .padding(16.0)
-                                .selector(Selector::from("container").class("menu"))
-                                .vertical_alignment("Center")
-                                .horizontal_alignment("Center")
-                                .child(
-                                    Stack::create()
-                                        .child(
-                                            TextBlock::create()
-                                                .selector(Selector::from("textblock").class("h1"))
-                                                .text("Dungeon")
-                                                .horizontal_alignment("Center")
-                                                .build(context),
-                                        )
-                                        .child(
-                                            Button::create()
-                                                .margin((0.0, 16.0, 0.0, 0.0))
-                                                .text("Start Game")
-                                                .on_click(move |_| {
-                                                    ng_state.start_game();
-                                                    true
-                                                })
-                                                .build(context),
-                                        )
-                                        .child(
-                                            Button::create()
-                                                .margin((0.0, 8.0, 0.0, 0.0))
-                                                .text("Quit")
-                                                .on_click(move |_| {
-                                                    q_state.quit_game();
-                                                    true
-                                                })
-                                                .build(context),
-                                        )
-                                        .build(context),
-                                )
-                                .build(context),
-                        )
-                        .build(context),
-                )
-        }
+        self.name("MenuView")
+            .selector(Selector::default().id("menu_view"))
+            .child(
+                Grid::create()
+                    .selector(Selector::from("grid").class("start"))
+                    .child(
+                        Container::create()
+                            .padding(16.0)
+                            .selector(Selector::from("container").class("menu"))
+                            .vertical_alignment("Center")
+                            .horizontal_alignment("Center")
+                            .child(
+                                Stack::create()
+                                    .child(
+                                        TextBlock::create()
+                                            .selector(Selector::from("textblock").class("h1"))
+                                            .text("Dungeon")
+                                            .horizontal_alignment("Center")
+                                            .build(context),
+                                    )
+                                    .child(
+                                        Button::create()
+                                            .margin((0.0, 16.0, 0.0, 0.0))
+                                            .text("Start Game")
+                                            .on_click(move |_| {
+                                                ng_state.start_game();
+                                                true
+                                            })
+                                            .build(context),
+                                    )
+                                    .child(
+                                        Button::create()
+                                            .margin((0.0, 8.0, 0.0, 0.0))
+                                            .text("Quit")
+                                            .on_click(move |_| {
+                                                q_state.quit_game();
+                                                true
+                                            })
+                                            .build(context),
+                                    )
+                                    .build(context),
+                            )
+                            .build(context),
+                    )
+                    .build(context),
+            )
     }
 }
 
-mod game {
-    use super::menu::*;
-    use orbgame::prelude::*;
+#[derive(Default)]
+pub struct GameViewState {}
 
-    #[derive(Default)]
-    struct GameViewState {}
-
-    impl State for GameViewState {
-        fn receive_messages(&self, context: &mut Context<'_>, messages: &Vec<MessageBox>) {
-            for message in messages {
-                if let Ok(message) = message.downcast_ref::<StringMessage>() {
-                    match message.0.as_str() {
-                        "start" => {
-                            if let Some(menu_view) = &mut context.child_by_id("menu_view") {
-                                menu_view.set::<Visibility>(Visibility::from("Collapsed"));
-                            }
+impl State for GameViewState {
+    fn receive_messages(&self, context: &mut Context<'_>, messages: &Vec<MessageBox>) {
+        for message in messages {
+            if let Ok(message) = message.downcast_ref::<StringMessage>() {
+                match message.0.as_str() {
+                    "start" => {
+                        if let Some(menu_view) = &mut context.child_by_id("menu_view") {
+                            menu_view.set::<Visibility>(Visibility::from("Collapsed"));
                         }
-                        "quit" => {
-                            context.push_event(SystemEvent::Quit);
-                        }
-                        _ => {}
                     }
+                    "quit" => {
+                        context.push_event(SystemEvent::Quit);
+                    }
+                    _ => {}
                 }
             }
         }
     }
+}
 
-    widget!(
+widget!(
         GameView<GameViewState> {
             selector: Selector
         }
     );
 
-    impl Template for GameView {
-        fn template(self, id: Entity, context: &mut BuildContext) -> Self {
-            let state = self.clone_state();
+impl Template for GameView {
+    fn template(self, id: Entity, context: &mut BuildContext) -> Self {
+        let state = self.clone_state();
 
-            self.name("GameView")
-                .selector(Selector::default().id("game_view"))
-                .child(
-                    Grid::create()
-                        .child(
-                            Container::create()
-                                .child(TextBlock::create().text("Dungeon").build(context))
-                                .build(context),
-                        )
-                        .child(MenuView::create().build(context))
-                        .build(context),
-                )
-        }
+        self.name("GameView")
+            .selector(Selector::default().id("game_view"))
+            .child(
+                Grid::create()
+                    .child(
+                        Container::create()
+                            .child(TextBlock::create().text("Dungeon").build(context))
+                            .build(context),
+                    )
+                    .child(MenuView::create().build(context))
+                    .build(context),
+            )
     }
 }
 
@@ -161,6 +151,6 @@ fn main() {
                 .extension_path("examples/res/dungeon/theme.css")
                 .build(),
         )
-        .build(game::GameView::create());
+        .build(GameView::create());
     game.run();
 }
